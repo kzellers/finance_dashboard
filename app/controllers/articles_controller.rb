aa68d8288855df79598eff913470caf06b424311
Karen Zellers <karen.zellers@gmail.com>
2020-05-13 18:00:48 -0700

    adding ability to view articles and navigate between pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,31 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end

f5999784ab1decb686a6330bb72a715a161c401c
Karen Zellers <karen.zellers@gmail.com>
2020-05-13 20:25:36 -0700

    added date input

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "karen", password: "password",
+except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -22,6 +26,23 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
